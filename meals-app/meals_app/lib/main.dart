3f005653adf9d6a7a90067c9162a5ce8a6b6d553
Aly Farag <afarag@Alys-MacBook-Pro.local>
2020-08-07 22:40:59 -0400

    added fonts, created second screen for categories,

--- meals-app/meals_app/lib/main.dart
+++ meals-app/meals_app/lib/main.dart
@@ -12,8 +12,17 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.green,
+          accentColor: Colors.green[400],
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       home: CategoriesScreen(),
     );
   }
