15237b01e234c11f3260efa9877a50bbd6f5bde0
Aly Farag <afarag@Alys-MacBook-Pro.local>
2020-10-29 23:16:08 -0400

    added home screen

--- gma_app/gma_app/lib/main.dart
+++ gma_app/gma_app/lib/main.dart
@@ -14,6 +14,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.green,
+          primaryColor: Color.fromRGBO(39, 174, 96, 1.0),
+          accentColor: Color.fromRGBO(46, 204, 113, 1.0),
           // This makes the visual density adapt to the platform that you run
           // the app on. For desktop platforms, the controls will be smaller and
           // closer together (more dense) than on mobile platforms.
--- gma_app/gma_app/lib/screens/home_screen.dart
+++ gma_app/gma_app/lib/screens/home_screen.dart
@@ -5,11 +5,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Grocery Management Application")),
-      body: Center(
-        child: Text(
-            "This is the home screen\n Here we will add 3 buttons 1 for add, 1 for current, 1 for completed."),
-      ),
-    );
+        appBar: AppBar(title: Text("Grocery Management Application")),
+        body: Card(
+          elevation: 10,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.add,
+                        color: Color.fromRGBO(41, 128, 185, 1.0)),
+                    label: Text("Add Grocery Lists")),
+                FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.list),
+                    label: Text("My Current Lists")),
+                FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.check_circle,
+                        color: Theme.of(context).accentColor),
+                    label: Text("My Completed Grocery Errands")),
+              ],
+            ),
+          ),
+        ));
   }
 }
